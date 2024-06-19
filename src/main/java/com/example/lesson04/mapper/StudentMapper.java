45b8c01c500970ee0a1295e4524c099024be308d
DESKTOP-C5GBA64\user <test@test.com>
2024-06-19 18:51:14 +0900

    2024-06-19

---
+++ src/main/java/com/example/lesson04/mapper/StudentMapper.java
@@ -0,0 +1,11 @@
+package com.example.lesson04.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.lesson04.domain.Student;
+
+@Mapper
+public interface StudentMapper {
+
+	public int insertStudent(Student student);
+}
